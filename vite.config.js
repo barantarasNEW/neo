16156a7990e8a1d304b3b5ec95147987b58b5c20
Taras <baran.taras.dev@gmail.com>
2023-07-06 10:23:54 +0200

    setuped project

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  define: {
+    'process.env': {
+      BASE_URL: "https://api.nasa.gov/neo/rest/v1/feed",
+      API_KEY: "PXjG2k4gTiQT1uLnemaLCDAX3RDa7jRbL69WIROx"
+    }
+  }
+})
